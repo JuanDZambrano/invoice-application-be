488ef08d846f197cec38ac27911134f27843668f
JuanDZambrano <zambra1809@gmail.com>
2023-07-03 11:09:13 -0500

    [Update] Create custom admin list showing all fields for every model

--- invoice_app/admin.py
+++ invoice_app/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
-from .models import (Product, 
-                     Contact, 
-                     Sale, 
-                     Debt, 
+from .models import (Product,
+                     Contact,
+                     Sale,
+                     Debt,
                      Employee)
 
-admin.site.register(Product)
-admin.site.register(Contact)
-admin.site.register(Sale)
-admin.site.register(Debt)
-admin.site.register(Employee)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Contact._meta.fields]
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Sale._meta.fields]
+
+
+class DebtAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Debt._meta.fields]
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Employee._meta.fields]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(Debt, DebtAdmin)
+admin.site.register(Employee, EmployeeAdmin)
