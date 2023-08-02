b484ea6745675b7f5bcd07152ee1870d7d13fd80
JuanDZambrano <zambra1809@gmail.com>
2023-08-02 11:37:45 -0500

    [Fix] missing schema for CRSF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ CORS_ALLOWED_ORIGINS = (
     "http://localhost:8000",
 )
 
-CSRF_TRUSTED_ORIGINS = ["localhost:5173"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
 
 ROOT_URLCONF = 'config.urls'
 
