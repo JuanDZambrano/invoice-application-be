ff8678741948033bcd90ed3fb7a0de881f8dbab1
JuanDZambrano <zambra1809@gmail.com>
2023-08-01 19:37:08 -0500

    [New] Create business models and tests

---
+++ business/constants.py
@@ -0,0 +1,25 @@
+JOB_TYPE_CHOICES = (
+    ('MG', 'Manager'),
+    ('SA', 'Sales Associate'),
+    ('AN', 'Analyst'),
+    ('OT', 'Other'),
+)
+
+BILL_TYPE_CHOICES = (
+    ('UT', 'Utilities'),
+    ('RE', 'Rent'),
+    ('SA', 'Salaries'),
+    ('OT', 'Other'),
+)
+
+BILL_STATUS_CHOICES = (
+    ('PE', 'Pending'),
+    ('CO', 'Completed'),
+    ('CA', 'Cancelled'),
+)
+
+ORDER_STATUS_CHOICES = (
+    ('PE', 'Pending'),
+    ('CO', 'Completed'),
+    ('CA', 'Cancelled'),
+)
---
+++ business/models.py
@@ -0,0 +1,223 @@
+import uuid
+
+from django.db import models
+
+from accounts.models import Company
+
+from .constants import (BILL_STATUS_CHOICES, BILL_TYPE_CHOICES,
+                        JOB_TYPE_CHOICES, ORDER_STATUS_CHOICES)
+
+
+class Employee(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='employees')
+    email = models.EmailField()
+    job = models.CharField(max_length=2, choices=JOB_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Employee: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Employees"
+
+
+class Location(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Location: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Locations"
+
+
+class Job(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+    remuneration = models.FloatField()
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='jobs')
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Job: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Jobs"
+
+
+class Customer(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField()
+    notes = models.TextField(blank=True)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='customers')
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Customer: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Customers"
+
+
+class Category(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Category: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    unit_price = models.FloatField()
+    inventory = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='products')
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Product: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+
+class Payment(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Payment: {self.name}>"
+
+    class Meta:
+        verbose_name_plural = "Payments"
+
+
+class Order(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='orders')
+    payment_method = models.ForeignKey(
+        Payment, on_delete=models.CASCADE, related_name='orders')
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(max_length=2, choices=ORDER_STATUS_CHOICES)
+    employee = models.ForeignKey(
+        Employee, null=True, blank=True, on_delete=models.SET_NULL, related_name='orders')
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+    def __repr__(self):
+        return f"<Order: {self.id}>"
+
+    class Meta:
+        verbose_name_plural = "Orders"
+
+
+class OrderItem(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    amount = models.IntegerField()
+    price = models.FloatField()
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='order_items')
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='order_items')
+
+    def __str__(self):
+        return f"Order Item {self.id}"
+
+    def __repr__(self):
+        return f"<OrderItem: {self.id}>"
+
+    class Meta:
+        verbose_name_plural = "Order Items"
+
+
+class Bill(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    due_date = models.DateTimeField()
+    amount = models.FloatField()
+    type = models.CharField(max_length=2, choices=BILL_TYPE_CHOICES)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='bills')
+    status = models.CharField(max_length=2, choices=BILL_STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount}"
+
+    def __repr__(self):
+        return f"<Bill: {self.type} - {self.amount}>"
+
+    class Meta:
+        verbose_name_plural = "Bills"
